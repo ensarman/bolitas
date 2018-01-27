2b01b9f2b860681ed86c1233ecea5a244b0abc60
Alvin Chunga Mamani <alvin@episunsa.edu.pe>
2018-01-27 14:05:47 -0500

    - Add Coins

--- Player1.gd
+++ Player1.gd
@@ -5,7 +5,7 @@ func _ready():
 	top_move_speed = 200
 	top_jump_speed = 300
 
-
+var f = 0;
 func apply_force(state):
 	if (Input.is_action_pressed("move_left")):
 		directional_force += DIRECTION.LEFT
@@ -13,3 +13,16 @@ func apply_force(state):
 		directional_force += DIRECTION.RIGHT
 	if (Input.is_action_pressed("move_jump")):
 		directional_force += DIRECTION.UP
+
+
+func _on_Bolita_body_enter( body ):
+	f+=1
+	var x2= f%3
+
+	get_node("Sprite").set_frame(x2);
+	update()
+	_change_notify()
+	get_node("Sprite").update()
+	get_node("Sprite")._change_notify()
+	print("asdsada")
+	pass # replace with function body
