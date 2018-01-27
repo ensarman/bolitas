6d4ceaca0f00d650a59509a6e18c7386a5f054ee
rmantilla <rmantilla@dlya.com.uy>
2018-01-27 11:33:38 -0500

    avance

---
+++ ControlPrincipal.gd
@@ -0,0 +1,5 @@
+extends Control
+func _ready():
+#	get_node("viewport").set_world_2d( get_node("top/viewport").get_world_2d() )
+#	get_node("top/viewport/stage/player2/camera").set_custom_viewport( get_node("viewport") )
+	pass
