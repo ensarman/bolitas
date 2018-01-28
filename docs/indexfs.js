18a946fa227301abbd658025cf2463410ecabc6a
Gustavo Puma <vash47@gmail.com>
2018-01-28 15:41:06 -0500

    versión web actualizada

--- docs/indexfs.js
+++ docs/indexfs.js
@@ -101,7 +101,7 @@ function assert(check, msg) {
         this.requests[this.name] = null;
       },
     };
-      new DataRequest(0, 30354686, 0, 0).open('GET', '/data.pck');
+      new DataRequest(0, 9002900, 0, 0).open('GET', '/data.pck');
     var PACKAGE_PATH;
     if (typeof window === 'object') {
       PACKAGE_PATH = window['encodeURIComponent'](window.location.pathname.toString().substring(0, window.location.pathname.toString().lastIndexOf('/')) + '/');
